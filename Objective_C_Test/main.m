717fe6f76b481a90b8e312df74d53a6c4ce0f3fb
Fizzw <102849884+Fizzw@users.noreply.github.com>
2023-03-01 15:01:32 +0900

    Initial Commit

---
+++ Objective_C_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective_C_Test
+//
+//  Created by 박정우 on 2023/03/01.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
